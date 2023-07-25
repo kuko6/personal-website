88899df2b24ba5922b8c950d94b53961f7f26fe9
Jakub Povinec <jakub.povinec@gmail.com>
2023-07-25 18:44:23 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import "../app.css";
+  import githubLogo from "../assets/github.svg";
+</script>
+
+<header>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/projects">Projects</a>
+    <a href="/blog">Blog</a>
+    <a href="https://github.com/kuko6">
+      <img alt="github logo" src={githubLogo}>
+    </a>
+  </nav>
+</header>
+
+<slot />
+
+<!-- <style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.blue.100);
+  }
+</style> -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+
+<div class="container mx-auto px-4">
+  <h1 class="font-inter font-black text-4xl">
+    Hello, I'm Jakub 👋
+  </h1>
+  <p class="font-inter font-medium text-base">
+    A computer science student from Bratislava, Slovakia. 
+  </p>
+  <h2 class="font-inter font-extrabold text-2xl">
+    About me
+  </h2>
+  <p>
+    Throughout my studies, I have developed a deep passion for mobile and web development, machine learning, and computer vision. 
+    While I’m still relatively new to the world of programming, I always try to expand my knowledge and skills and constantly seek new challenges and opportunities to grow.
+    Outside of programming, I enjoy learning new languages (currently studying French 🇫🇷), visiting art galleries and playing tennis or skiing.
+  </p>
+</div>
