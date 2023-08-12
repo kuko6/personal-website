c1f9cd8b7962b69c2a94754fa9950ef538b50346
Jakub Povinec <jakub.povinec@gmail.com>
2023-08-12 08:37:23 +0200

    added confetti on birthday :)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,9 @@
   function foundEasterEgg() {
     easterEgg = !easterEgg;
   }
+
+  let currentDate = new Date();
+  let targetDate = new Date(currentDate.getFullYear(), 9, 27);
 </script>
 
 <header>
@@ -18,20 +21,20 @@
     <div class="confetti">
       <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[0, 10000]} duration=5000 amount=300 iterationCount=infinite fallDistance="100vh" /> 
     </div>
+  {:else if currentDate.toDateString() === targetDate.toDateString()}
+    <div class="confetti">
+      <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[0, 10000]} duration=5000 amount=300 iterationCount=2 fallDistance="100vh" /> 
+    </div>
   {/if}
   <slot />
 </main>
 
 <footer class="fixed bottom-0 w-full my-10">
   <div class="flex items-center justify-center text-gray-400 dark:text-gray-500">
-    <p>
-      <a class="hover:underline underline-offset-2 decoration-blue-400" 
-        href="https://github.com/kuko6/kuko">made by Kuko,
-      </a> 
-      <span class="group" role="button" on:click={foundEasterEgg}>
-        2023
-      </span>
-    </p> 
+    <a class="hover:underline underline-offset-2 decoration-blue-400" 
+      href="https://github.com/kuko6/kuko">made by Kuko
+    </a> 
+    <span class="group" role="button" on:click={foundEasterEgg}>, 2023</span>
   </div>
 </footer>
 
