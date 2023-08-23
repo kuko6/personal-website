91187821fe489d57e29d45a151e8934da08467d6
Jakub Povinec <jakub.povinec@gmail.com>
2023-08-23 17:59:53 +0200

    fixed responsivness issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,31 +12,34 @@
   let targetDate = new Date(currentDate.getFullYear(), 9, 27);
 </script>
 
-<header>
-  <Navigation />
-</header>
 
-<main class="flex-1 h-full">
-  {#if easterEgg}
-    <div class="confetti">
-      <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[0, 10000]} duration=5000 amount=300 iterationCount=infinite fallDistance="100vh" /> 
+<div class="flex flex-col min-h-screen">
+  <header>
+    <Navigation />
+  </header>
+  
+  <main class="flex-grow">
+    {#if easterEgg}
+      <div class="confetti">
+        <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[0, 10000]} duration=5000 amount=300 iterationCount=infinite fallDistance="100vh" /> 
+      </div>
+    {:else if currentDate.toDateString() === targetDate.toDateString()}
+      <div class="confetti">
+        <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[0, 10000]} duration=5000 amount=300 iterationCount=2 fallDistance="100vh" /> 
+      </div>
+    {/if}
+    <slot />
+  </main>
+ 
+  <footer class="py-10">
+    <div class="container mx-auto flex items-center justify-center text-gray-400 dark:text-gray-500">
+      <a class="hover:underline underline-offset-2 decoration-blue-400" 
+        href="https://github.com/kuko6/kuko">made by Kuko
+      </a> 
+      <span class="group" role="button" on:click={foundEasterEgg}>, 2023</span>
     </div>
-  {:else if currentDate.toDateString() === targetDate.toDateString()}
-    <div class="confetti">
-      <Confetti x={[-5, 5]} y={[0, 0.1]} delay={[0, 10000]} duration=5000 amount=300 iterationCount=2 fallDistance="100vh" /> 
-    </div>
-  {/if}
-  <slot />
-</main>
-
-<footer class="fixed bottom-0 w-full my-10">
-  <div class="flex items-center justify-center text-gray-400 dark:text-gray-500">
-    <a class="hover:underline underline-offset-2 decoration-blue-400" 
-      href="https://github.com/kuko6/kuko">made by Kuko
-    </a> 
-    <span class="group" role="button" on:click={foundEasterEgg}>, 2023</span>
-  </div>
-</footer>
+  </footer>
+</div>
 
 <style lang="postcss">
   .confetti {
