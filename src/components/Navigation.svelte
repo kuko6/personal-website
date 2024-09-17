8433063ecaa0003adf0dd9d4b72a42eba8823c3d
Jakub Povinec <jakub.povinec@gmail.com>
2024-09-17 11:17:31 +0200

    added resume

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,20 +1,23 @@
 <script>
   import GithubLogo from "./GithubLogo.svelte";
-  import LightModeButton from "./LightModeButton.svelte"; 
+  import LightModeButton from "./LightModeButton.svelte";
   import DarkModeButton from "./DarkModeButton.svelte";
   import NavLink from "../components/NavLink.svelte";
   import NavIcon from "../components/NavIcon.svelte";
 </script>
 
 <nav class="pt-5 md:pt-10 dark:text-white">
-  <div class="flex flex-row justify-end content-center text-base gap-2 font-inter font-bold">
+  <div
+    class="flex flex-row justify-end content-center text-base gap-2 font-inter font-bold"
+  >
     <NavLink label={"Home"} path={"/"} />
+    <!-- <NavLink label={"Blog"} path={"/blog"} /> -->
     <NavLink label={"Projects"} path={"/projects"} />
-   <!--  <NavLink label={"Blog"} path={"/blog"} />  -->
-    <NavIcon alt={"github logo"} path={"https://github.com/kuko6"}> 
-      <GithubLogo/>
-    </NavIcon> 
-    <div class="border-l border-black dark:border-white my-2"/>
+    <NavLink label={"Resume"} path={"/cv.pdf"} />
+    <NavIcon alt={"github logo"} path={"https://github.com/kuko6"}>
+      <GithubLogo />
+    </NavIcon>
+    <div class="border-l border-black dark:border-white my-2" />
     <LightModeButton />
     <DarkModeButton />
   </div>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>Blog - Kuko</title>
+</svelte:head>
+<div class="space-y-6 dark:text-white">
+  <h1 class="font-inter font-black text-2xl md:text-4xl">Blog</h1>
+  <h2>Working on it... 👷</h2>
+</div>
